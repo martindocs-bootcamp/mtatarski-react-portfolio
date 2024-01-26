7fbd14c7cc8615cac7d7482e3b57242dc2d59ff4
Marcin Tatarski <martin.tatarski>
2024-01-26 22:30:37 +0000

    Add navigation

---
+++ portfolio/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/images/logo.png';
+
+const Logo = () => {
+  return (
+    <></>
+    // <img src={logo} alt="Website M and T logo" className='logo' />
+  )
+}
+
+export default Logo;
