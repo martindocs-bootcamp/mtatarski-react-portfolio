7a20d0a0944b9907b0fcd82a9cdfc79a1b375394
Marcin Tatarski <martin.tatarski>
2024-01-27 19:59:48 +0000

    Add Contact page

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,7 +1,62 @@
+import {useState} from 'react';
 
 const Contact = () => {
+  const [prop, setProp] = useState({
+    email: '',
+    message: '',
+  })
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleClear = () => {
+    setProp({
+      email: '',
+      message: '',
+    })
+  }
+
+  const handleInputChange = (e) => {
+    const{name, value} = e.target;
+    setProp({...prop, [name]: value});
+  }
+
   return (
-    <div>Contact</div>
+    <form className="contact" onSubmit={(e)=> e.preventDefault()}>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">Email address</label>
+        <input 
+          type="email" 
+          className="form-control" 
+          id="email" 
+          name="email"
+          value={prop.email}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="message" className="form-label">Message</label>
+        <textarea 
+          className="form-control" 
+          id="message" 
+          rows="3"
+          name="message"
+          value={prop.message} 
+          onChange={handleInputChange}
+        ></textarea>
+      </div>
+      <div className="buttons">
+        <div 
+          className="btn btn-primary submit"
+          onClick={handleSubmit}
+        >Submit</div>
+        <div 
+          className="btn btn-danger clear"
+          onClick={handleClear}
+        >Clear</div>
+      </div>
+    </form>
   )
 }
 
