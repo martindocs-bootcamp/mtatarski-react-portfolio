84322ea62fbcfd84dcca53ac8e61a6aa828467d8
Marcin Tatarski <martin.tatarski>
2024-01-27 11:16:36 +0000

    Add project card

---
+++ portfolio/src/components/Card.jsx
@@ -0,0 +1,33 @@
+import {PropTypes} from 'prop-types';
+
+const Card = ({
+  image, 
+  title, 
+  description, 
+  link_live, 
+  link_code
+}) => {
+  return (
+    <div className="card card-custom">     
+      <img src={image} className="card-img-top" alt={image} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <div className="card-footer">
+          <a href={link_live} className="btn btn-primary card-btn-live">Live</a>          
+          <a href={link_code} className="btn btn-secondary card-btn-code">Code</a>          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Card.propTypes = {
+  image: PropTypes.string.isRequired, 
+  title: PropTypes.string.isRequired, 
+  description: PropTypes.string.isRequired, 
+  link_live: PropTypes.string.isRequired, 
+  link_code: PropTypes.string.isRequired
+}
+
+export default Card;
--- portfolio/src/components/index.jsx
+++ portfolio/src/components/index.jsx
@@ -3,11 +3,13 @@ import ErrorPage from "./ErrorPage";
 import Home from "./Home";
 import Navigation from "./Navigation";
 import Projects from "./Projects";
+import Card from "./Card";
 
 export {
   Contact,
   ErrorPage,
   Home,
   Navigation,
-  Projects
+  Projects,
+  Card,
 }
