793c42c0948819427b2657bd00e3ca2f86beea35
Marcin Tatarski <martin.tatarski>
2024-01-28 20:08:39 +0000

    Add styling to home page

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -8,50 +8,53 @@ const Home = () => {
   }
 
   return (
-    <div className="card about">
+    <div className="card home">
       <div className="row">      
-        <div className="col">
+        <div className="col col-md-4 d-flex justify-content-center align-items-center">
           <img 
             src={profileImg} 
-            className="card-img-top about-img" 
+            className="card-img-top home-img" 
             alt="Photo of Marcin Tatarski on a white background" 
           />
         </div>
-        <div className="col">
-          <div className="card-header about-header">Marcin Tatarski</div>
-          <div className="card-body about-content">
-            <h3 className='about-statement'>Some text</h3>
-            <div className='about-location'>
-              <i className="fa-solid fa-location-dot"></i> 
-              <p>Worcester, UK</p>
+        <div className="col col-md-8">
+          <div className="card-header home-header">Marcin Tatarski</div>
+          <div className="card-body home-content">
+            <h3 className='home-tagline'>Designing Experiences, Not Just Interfaces.</h3>            
+            <div className='home-location d-flex align-items-baseline'>
+              <i className="fa-solid fa-location-dot home-icon"></i> 
+              <div>Worcester, UK</div>
             </div>
-            <div className='about-email'>
-              <i className="fa-regular fa-envelope"></i> 
+            <div className='home-email d-flex align-items-baseline'>
+              <i className="fa-regular fa-envelope home-icon"></i> 
               <a href="mailo:martin.tatarski@gmail.com">martin.tatarski@gmail.com</a>
             </div>
-            <div className='about-resume'>
-              <i className="fa-regular fa-file"></i>
+            <div className='home-resume d-flex align-items-baseline'>
+              <i className="fa-regular fa-file home-icon"></i>
               <button onClick={openPdfInNewTab}>Resume</button>              
             </div>           
-            <ul className='about-skills'>
-              <li className='about-skill'>React</li>
-              <li className='about-skill'>Node.js</li>
-              <li className='about-skill'>Boostrali</li>
-              <li className='about-skill'>Exliress.js</li>
-              <li className='about-skill'>MongoDB</li>
-              <li className='about-skill'>CSS3</li>              
+            <ul className='home-skills'>
+              <li className='home-skill'>React</li>
+              <li className='home-skill'>Node.js</li>
+              <li className='home-skill'>Boostrali</li>
+              <li className='home-skill'>Exliress.js</li>
+              <li className='home-skill'>MongoDB</li>
+              <li className='home-skill'>CSS3</li>              
             </ul>
-            <div className='about-btn'>
-              <button className='btn about-btn-github'><i className="fa-brands fa-github"></i></button>
-              <button className='btn about-btn-linkedin'><i className="fa-brands fa-linkedin"></i></button>
+            <div className='home-btn'>
+              <button className='btn home-btn-github' title="GitHub"><i className="fa-brands fa-github"></i></button>
+              <button className='btn home-btn-linkedin' title="Linkedin"><i className="fa-brands fa-linkedin"></i></button>
             </div>
           </div>          
         </div>
       </div>
-      <div className="row about-me">
+      <div className="row home-me">
         <div className="col">
-            <p className='about-me-txt'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi ducimus voluptate expedita dolorum asperiores odit impedit perspiciatis mollitia repellat molestias.
+            <p className='home-me-text'>
+            Hi there, I&apos;m Marcin, a passionate self-taught web developer.              
+            </p>
+            <p className='home-me-text'>
+            I have extensive experience in developing user-friendly websites and applications using the latest web technologies, such as the MERN stack. I&apos;ve honed my skills in front-end development, back-end development, and database management. I&apos;m also well-versed in Agile methodologies and always strive to deliver projects on time and within budget. When I&apos;m not coding, I enjoy exploring new technologies and pushing myself to learn more. I&apos;m excited about the opportunity to leverage my skills to develop innovative solutions. 
             </p>
         </div>
       </div>
