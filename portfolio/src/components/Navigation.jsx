5d0089dd806fe7014629cd5f5808f01540590670
Marcin Tatarski <martin.tatarski>
2024-01-28 23:53:08 +0000

    Add favicon and logo

--- portfolio/src/components/Navigation.jsx
+++ portfolio/src/components/Navigation.jsx
@@ -5,7 +5,7 @@ const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
           <Logo />
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
