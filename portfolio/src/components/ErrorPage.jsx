3eb353c71d4544514fe84ee52ca01df7f5b973f2
Marcin Tatarski <martin.tatarski>
2024-01-29 13:57:08 +0000

    Update error 404 page

--- portfolio/src/components/ErrorPage.jsx
+++ portfolio/src/components/ErrorPage.jsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+import errorImg from '../assets/svg/error404.svg';
 
+// ErrorPage component to be displayed for 404 error
 const ErrorPage = () => {
+
+  // Render the error page content
   return (
-    <div>ErrorPage</div>
+    <div className="row">
+      <div className="col col-md-6 mx-auto error">            
+        <h2 className='error-header'>Oops... Page Not Found</h2>
+        <img src={errorImg} alt="error 404 message" className='error-img' />
+        <p>We can&apos;t seem to find the page you&apos;re looking for</p>
+
+        {/* Link to navigate back to the home page */}
+        <Link to="/" className='error-home'>back home</Link>     
+      </div>
+    </div>
   )
 }
 
